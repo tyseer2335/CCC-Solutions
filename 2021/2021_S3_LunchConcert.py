2e9b5991d6537d8cf00e3c1685e6e8d4f78ab09f
Tyseer Toufiq <you@example.com>
2022-01-21 21:26:22 -0500

    Completed S3 for CCC 2021

---
+++ 2021/2021_S3_LunchConcert.py
@@ -0,0 +1,56 @@
+Friends = int(input()) 
+Data = [] 
+
+for i in range(Friends): 
+    Data.append(input().split())
+
+def SumFinder(Guess): # Function finds the Sum of the walking times
+    BooleanArr = []  
+    Sum = 0
+
+    for i in Data:  
+        if Guess in range(int(i[0])-int(i[2]), int(i[0])+int(i[2])): 
+            BooleanArr.append(True) 
+        else:   
+            BooleanArr.append(False)
+
+    for i in enumerate(Data): 
+        if BooleanArr[i[0]] == False: 
+            if Guess>int(i[1][0]): 
+                Sum += ( Guess - (int(i[1][0]) + int(i[1][2])) ) * (int(i[1][1]))
+            elif Guess<int(i[1][0]):  
+                Sum += ((int(i[1][0]) - int(i[1][2]) - Guess) * (int(i[1][1])))
+    return Sum
+    
+Low = 0
+High = 1000000000 
+Choice = 0 
+Mid = 0
+MinSum = 0 
+Left = 0 
+Right = 0
+
+while (Low<=High): 
+    Mid = (Low+High)//2 
+    MinSum = SumFinder(Mid)  
+    Left = SumFinder(Mid-1)   
+    Right = SumFinder(Mid+1)  
+    if (MinSum<=Right and MinSum<=Left): 
+        break 
+    elif (MinSum<Right): 
+        High = Mid-1 
+    elif (MinSum<Left): 
+        Low = Mid + 1
+    
+print(MinSum)
+
+# CCC 15/15  
+# DMOJ 4/15 ??
+
+
+    
+
+
+
+
+
